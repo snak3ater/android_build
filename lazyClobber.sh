80cf913d14e17763f6440c9b60e48a5a110165f5
rascarlo <rascarlo@gmail.com>
2013-12-13 09:04:31 +0100

    lazy sh changes

--- lazyClobber.sh
+++ lazyClobber.sh
@@ -2,11 +2,11 @@
 make clobber
 make -j12
 source build/envsetup.sh
-# mako
-lunch rascarlo_mako-user && make otapackage -j12
-# manta
-lunch rascarlo_manta-user && make otapackage -j12
-# grouper
-lunch rascarlo_grouper-user && make otapackage -j12
 # hammerhead
 lunch rascarlo_hammerhead-user && make otapackage -j12
+# manta
+lunch rascarlo_manta-user && make otapackage -j12
+# mako
+lunch rascarlo_mako-user && make otapackage -j12
+# grouper
+lunch rascarlo_grouper-user && make otapackage -j12
--- lazyMine.sh
+++ lazyMine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source build/envsetup.sh
 make installclean
-# mako
-lunch rascarlo_mako-user && make installclean && make otapackage -j12
+# hammerhead
+lunch rascarlo_hammerhead-user && make installclean && make otapackage -j12
 # manta
 lunch rascarlo_manta-user && make installclean && make otapackage -j12
